038fd254ba90ef21c4c0dc163443ea6129ea4e57
Tim Pickles <tim.pickles@snyk.io>
2025-12-01 15:16:13 +0000

    feat: ignore common folders matching legacy test

--- pkg/ecosystems/discovery/common_excludes.go
+++ pkg/ecosystems/discovery/common_excludes.go
@@ -1,32 +1,6 @@
 package discovery
 
 var commonExcludes = []string{
-	// Any hidden folder
-	".*",
-
-	// Common build directories
-	"dist",
-	"build",
-
-	// JavaScript/Node.js
+	".build",
 	"node_modules",
-	".next",
-	".nuxt",
-
-	// Python (pip, poetry, uv)
-	"__pycache__",
-	"*.egg-info",
-	"*.dist-info",
-	"venv",
-	"env",
-
-	// Go
-	"vendor",
-
-	// Ruby (Bundler)
-	".bundle",
-
-	// Swift/iOS
-	"Pods",
-	"Carthage",
 }
